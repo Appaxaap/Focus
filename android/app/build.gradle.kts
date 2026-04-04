2bd63d862f8f70347903e93a06b20ea71aabdca2
Basim Basheer <basimbasheer001@gmail.com>
2026-04-04 23:14:31 +0530

    fix: remove splits block conflicting with Flutter --split-per-abi flag

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -77,17 +77,6 @@ android {
         }
     }
 
-    // SPLIT PER ABI CONFIGURATION
-    splits {
-        abi {
-            isEnable = true
-            reset()
-            include("armeabi-v7a", "arm64-v8a", "x86_64")
-            // This will generate split APKs when using --split-per-abi
-            isUniversalApk = true
-        }
-    }
-
     dependenciesInfo {
         includeInApk = false
         includeInBundle = false
